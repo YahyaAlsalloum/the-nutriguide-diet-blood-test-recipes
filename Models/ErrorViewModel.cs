44f16370e065ece88ac3343f1d53987512d545f4
YahyaAlsalloum <yahya.moh.sal@gmail.com>
2024-05-23 13:44:05 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace the_nutriguide.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
